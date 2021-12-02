773ca70f038fcb2377b0ae66d39ed90d7b065ef6
jerone-1997 <1902606@sit.singaporetech.edu.sg>
2021-12-02 15:39:27 +0800

    Update button_process.php

--- src/button_process.php
+++ src/button_process.php
@@ -11,7 +11,7 @@ if (isset($_POST['action'])) {
 
         } else {
             $success = false;
-            $_SESSION['hello'] = $checkTheValue;
+            $_SESSION['hello'] = $_POST["formValue"];
         }
 
         
